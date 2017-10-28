0047296fdc7820c000c53397d8eb91f7af7b4fa7
ammad <ammad@10.0.0.22>
2017-10-28 13:27:38 -0500

    Created Category Command Object.

---
+++ src/main/java/Recipe/JpaHibernateDemo/Commands/CategoryCommanClass.java
@@ -0,0 +1,41 @@
+package Recipe.JpaHibernateDemo.Commands;
+
+import java.util.Set;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+import Recipe.JpaHibernateDemo.Entities.Recipe;
+// A Command Object for Category Entity
+public class CategoryCommanClass {
+	    private Long id;
+	    private String description;  
+	    public Long getId() {
+
+	        return id;
+
+	    }
+	    public void setId(Long id) {
+
+	        this.id = id;
+
+	    }
+
+	    public String getDescription() {
+
+	        return description;
+
+	    }
+
+	    public void setDescription(String description) {
+
+	        this.description = description;
+
+	    }
+
+
+	
+	
+}
