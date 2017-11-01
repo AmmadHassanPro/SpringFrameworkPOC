c8c1bbf1b19a5eb451cd530b75e49a488c779523
ammad <ammad@10.0.0.22>
2017-10-31 23:15:51 -0500

    Added convertToCategorySet in CategoryCommandToCategoryEntity. to return Set of Entities

--- src/main/java/Recipe/JpaHibernateDemo/CommandConverters/CategoryCommandToCategoryEntity.java
+++ src/main/java/Recipe/JpaHibernateDemo/CommandConverters/CategoryCommandToCategoryEntity.java
@@ -1,14 +1,19 @@
 package Recipe.JpaHibernateDemo.CommandConverters;
 
+import java.util.List;
+import java.util.Set;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
 import Recipe.JpaHibernateDemo.Commands.CategoryCommand;
 import Recipe.JpaHibernateDemo.Entities.Category;
 @Component
-public class CategoryCommandToCategoryEntityConvertor implements Converter<CategoryCommand, Category>{
+public class CategoryCommandToCategoryEntity implements Converter<CategoryCommand, Category>{
 	@Autowired
 	private Category categoryEntity;
+	@Autowired
+	private Set<Category> categoryEntityList;
 	@Override
 	public Category convert(CategoryCommand categoryCommand) {
 		if(categoryCommand==null)
@@ -17,5 +22,18 @@ public class CategoryCommandToCategoryEntityConvertor implements Converter<Categ
 		this.categoryEntity.setId(categoryCommand.getId());
 		return this.categoryEntity;
 	}
+	//Method for converting List of CategoryCommands to List of Category Entities
+	public Set<Category> convertToCategorySet(Set<CategoryCommand> categoryCommandList) {
+		if(categoryCommandList==null)
+		{return null;}
+		for(CategoryCommand cc: categoryCommandList) {
+		categoryEntityList.add(this.convert(cc));
+		}
+		
+		return categoryEntityList;
+	}
+	
+	
+	
 	
 }
