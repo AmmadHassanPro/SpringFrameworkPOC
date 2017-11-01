501cf803baad3d11333b0a04b03e6e24514d780c
ammad <ammad@10.0.0.22>
2017-10-31 23:16:40 -0500

    Added Recipe Command Converters to Entity and vice versa.

---
+++ src/main/java/Recipe/JpaHibernateDemo/CommandConverters/RecipeCommandToRecipeEntity.java
@@ -0,0 +1,48 @@
+package Recipe.JpaHibernateDemo.CommandConverters;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.convert.converter.Converter;
+
+import Recipe.JpaHibernateDemo.Commands.NotesCommand;
+import Recipe.JpaHibernateDemo.Commands.RecipeCommand;
+import Recipe.JpaHibernateDemo.Entities.Notes;
+import Recipe.JpaHibernateDemo.Entities.Recipe;
+
+
+public class RecipeCommandToRecipeEntity implements Converter<RecipeCommand,Recipe>{
+@Autowired
+private Recipe recipeEntity;
+@Autowired
+private CategoryCommandToCategoryEntity cctcec;
+@Autowired
+private IngredientCommandToIngredientEntity ictiec;
+@Autowired
+private NotesCommandToNotesEntity nctne;
+	@Override
+	public Recipe convert(RecipeCommand recipeCommand) {
+		if(recipeCommand == null) {
+		return null;
+		}
+		recipeEntity.setDescription(recipeCommand.getDescription());
+		recipeEntity.setCookTime(recipeCommand.getCookTime());
+		recipeEntity.setPrepTime(recipeCommand.getPrepTime());
+		recipeEntity.setDescription(recipeCommand.getDescription());
+		recipeEntity.setDifficulty(recipeCommand.getDifficulty());
+		recipeEntity.setDirections(recipeCommand.getDirections());
+		recipeEntity.setName(recipeCommand.getName());
+		recipeEntity.setServings(recipeCommand.getServings());
+		recipeEntity.setSource(recipeCommand.getSource());
+		recipeEntity.setUrl(recipeCommand.getUrl());
+		//Converting List of CategoryCommands to List of Category Entities and setting it in recipeEntity
+		recipeEntity.setCategories(cctcec.convertToCategorySet(recipeCommand.getCategories()));
+		//Converting List of Ingredient Commands to List of Ingredient Entities and setting it in recipeEntity
+		recipeEntity.setIngredients(ictiec.convertToIngredientSet(recipeCommand.getIngredients()));
+		//Converting Notes Command to Notes Entity and setting it in recipeEntity
+		recipeEntity.setRecipeNotes(nctne.convert(recipeCommand.getRecipeNotes()));
+		
+		return recipeEntity;
+		
+		
+	}
+
+}
