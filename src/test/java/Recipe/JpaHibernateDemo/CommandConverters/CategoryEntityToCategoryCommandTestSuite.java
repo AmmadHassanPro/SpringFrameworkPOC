5848fe3f77388ff70f27b9974afa3fb6d5b4bc70
ammad <ammad@10.0.0.22>
2017-11-05 12:24:02 -0600

    Created CategoryCommandToCategoryEntityTestSuite

---
+++ src/test/java/Recipe/JpaHibernateDemo/CommandConverters/CategoryEntityToCategoryCommandTestSuite.java
@@ -0,0 +1,40 @@
+package Recipe.JpaHibernateDemo.CommandConverters;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import Recipe.JpaHibernateDemo.Commands.CategoryCommand;
+import Recipe.JpaHibernateDemo.Entities.Category;
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class CategoryEntityToCategoryCommandTestSuite {
+@Autowired 
+private CategoryEntityToCategoryCommand cetcc;
+@Autowired
+private Category categoryEntity;
+private CategoryCommand categoryCommand;
+
+
+@Before
+public void setUp() {
+	
+	categoryEntity.setId(1L);
+	categoryEntity.setDescription("Test Description");
+	
+}
+	@Test
+	public void testConvert() {
+		categoryCommand = cetcc.convert(categoryEntity);
+		
+		assertEquals(categoryCommand.getId(),categoryEntity.getId());
+		assertEquals(categoryCommand.getDescription(),categoryEntity.getDescription());
+		
+	}
+
+}
