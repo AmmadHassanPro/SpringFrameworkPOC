546ddb369d0a18d00aa5a008fce646f7a35dd321
ammad <ammad@10.0.0.22>
2017-11-05 11:29:32 -0600

    Changed Logic of the convertToCategorySet in CategoryCommandToCategoryEntity

--- src/main/java/Recipe/JpaHibernateDemo/CommandConverters/CategoryCommandToCategoryEntity.java
+++ src/main/java/Recipe/JpaHibernateDemo/CommandConverters/CategoryCommandToCategoryEntity.java
@@ -1,5 +1,6 @@
 package Recipe.JpaHibernateDemo.CommandConverters;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
@@ -12,25 +13,26 @@ import Recipe.JpaHibernateDemo.Entities.Category;
 public class CategoryCommandToCategoryEntity implements Converter<CategoryCommand, Category>{
 	@Autowired
 	private Category categoryEntity;
-	@Autowired
-	private Set<Category> categoryEntityList;
+	private Set<Category> categoryEntitySet;
 	@Override
 	public Category convert(CategoryCommand categoryCommand) {
 		if(categoryCommand==null)
 		{return null;}
+		categoryEntity = new Category();
 		this.categoryEntity.setDescription(categoryCommand.getDescription());
 		this.categoryEntity.setId(categoryCommand.getId());
 		return this.categoryEntity;
 	}
 	//Method for converting List of CategoryCommands to List of Category Entities
 	public Set<Category> convertToCategorySet(Set<CategoryCommand> categoryCommandList) {
+		categoryEntitySet = new HashSet<Category>();
 		if(categoryCommandList==null)
 		{return null;}
 		for(CategoryCommand cc: categoryCommandList) {
-		categoryEntityList.add(this.convert(cc));
+		this.categoryEntitySet.add(this.convert(cc));
 		}
 		
-		return categoryEntityList;
+		return this.categoryEntitySet;
 	}
 	
 	
