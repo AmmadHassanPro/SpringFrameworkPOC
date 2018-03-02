c9e4c54db4f35cb712031821947157ec28bb15a9
ammadhassan <ammadhassan@10.0.0.9>
2018-03-01 21:36:15 -0600

    Added Null Check in Image Converter

--- src/main/java/Recipe/JpaHibernatDemo/Utilities/ImageConverter.java
+++ src/main/java/Recipe/JpaHibernatDemo/Utilities/ImageConverter.java
@@ -11,14 +11,18 @@ public class ImageConverter {
 	
 	
 public byte[] ConvertToByteArray(String Url) {
+	ByteArrayOutputStream baos =new ByteArrayOutputStream();
+		if (Url == null || Url.isEmpty()) {
+			
+			return baos.toByteArray();
+		}
 		
-		ByteArrayOutputStream baos =new ByteArrayOutputStream();;
 		try {
 			URL imageURL = new URL(Url);
 			BufferedImage originalImage=ImageIO.read(imageURL);
 			ImageIO.write(originalImage, "jpg", baos );
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
+			
 			e.printStackTrace();
 		}
 		
