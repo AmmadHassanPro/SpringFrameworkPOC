9199677e0b80715a91ec87ae26259e3c54f4d164
ammadhassan <ammadhassan@10.0.0.178>
2018-09-22 23:05:25 -0500

    Fixed Issue in Native Sql Query
    It was not returning resultset when multiple records found in Sub Query.

--- src/main/java/Recipe/JpaHibernateDemo/Repository/RecipeRepository.java
+++ src/main/java/Recipe/JpaHibernateDemo/Repository/RecipeRepository.java
@@ -9,7 +9,7 @@ import org.springframework.data.repository.query.Param;
 import Recipe.JpaHibernateDemo.Entities.Recipe;
 
 public interface RecipeRepository extends CrudRepository<Recipe,Long>{
-	@Query(value="SELECT * FROM RECIPE WHERE ID= (SELECT RECIPE_ID FROM RECIPE_CATEGORY WHERE CATEGORY_ID= (SELECT ID FROM CATEGORY  WHERE DESCRIPTION = :category)) " , nativeQuery=true)
+	@Query(value="SELECT * FROM RECIPE WHERE ID IN (SELECT RECIPE_ID FROM RECIPE_CATEGORY WHERE CATEGORY_ID= (SELECT ID FROM CATEGORY  WHERE DESCRIPTION = :category)) " , nativeQuery=true)
 	List<Recipe> findbyCategory(@Param("category") String Category);
 
 }
