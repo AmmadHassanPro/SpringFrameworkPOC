533a3bce616e7a8a2acfe738778a9c93e52037ca
ammadhassan <ammadhassan@10.0.0.178>
2018-09-22 21:48:57 -0500

    Added Native Sql query in Recipe Repository to get Recipe by category

--- src/main/java/Recipe/JpaHibernateDemo/Repository/RecipeRepository.java
+++ src/main/java/Recipe/JpaHibernateDemo/Repository/RecipeRepository.java
@@ -1,9 +1,15 @@
 package Recipe.JpaHibernateDemo.Repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import Recipe.JpaHibernateDemo.Entities.Recipe;
 
 public interface RecipeRepository extends CrudRepository<Recipe,Long>{
+	@Query(value="SELECT * FROM RECIPE WHERE ID= (SELECT RECIPE_ID FROM RECIPE_CATEGORY WHERE CATEGORY_ID= (SELECT ID FROM CATEGORY  WHERE DESCRIPTION = :category)) " , nativeQuery=true)
+	List<Recipe> findbyCategory(@Param("category") String Category);
 
 }
