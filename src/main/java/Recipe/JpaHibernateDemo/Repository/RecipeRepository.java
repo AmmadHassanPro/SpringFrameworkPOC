789999a3ec22783f11cf3d978c5028e00c6f493e
ammadhassan <ammadhassan@10.0.0.178>
2018-09-25 21:47:46 -0500

    Modified the search query
    Modified the query in recipe repo to provide better result when
    searching

--- src/main/java/Recipe/JpaHibernateDemo/Repository/RecipeRepository.java
+++ src/main/java/Recipe/JpaHibernateDemo/Repository/RecipeRepository.java
@@ -11,7 +11,7 @@ import Recipe.JpaHibernateDemo.Entities.Recipe;
 public interface RecipeRepository extends CrudRepository<Recipe,Long>{
 	@Query(value="SELECT * FROM RECIPE WHERE ID IN (SELECT RECIPE_ID FROM RECIPE_CATEGORY WHERE CATEGORY_ID= (SELECT ID FROM CATEGORY  WHERE DESCRIPTION = :category)) " , nativeQuery=true)
 	public List<Recipe> findbyCategory(@Param("category") String Category);
-	@Query(value="SELECT * FROM RECIPE WHERE NAME LIKE CONCAT('%',:searchTerm,'%') OR DESCRIPTION LIKE CONCAT('%',:searchTerm,'%') " , nativeQuery=true)
+	@Query(value="SELECT * FROM RECIPE WHERE LOWER(NAME) LIKE LOWER(CONCAT('%',:searchTerm,'%')) OR LOWER(DESCRIPTION) LIKE LOWER(CONCAT('%',:searchTerm,'%')) " , nativeQuery=true)
 	public List<Recipe> findByNameOrDesc( @Param ("searchTerm")String searchTerm);
 
 }
